89520a96408ed4e95f78de85301de922020784ad
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2018-01-01 14:50:54 +0700

    Release v0.001

--- lib/Sah/Schema/hexbuf.pm
+++ lib/Sah/Schema/hexbuf.pm
@@ -3,7 +3,7 @@ package Sah::Schema::hexbuf;
 # DATE
 # VERSION
 
-our $schema => [str => {
+our $schema = [str => {
     summary => 'Binary data encoded in hexdigits',
     match => qr/\A[0-9A-fa-f]+\z/,
 }, {}];
