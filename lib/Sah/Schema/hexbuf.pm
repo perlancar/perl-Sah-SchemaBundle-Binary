c576394eddec691712146a4777ae994ca6323838
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-03-02 02:29:24 +0700

    Tweak regex: allow empty string & disallow odd number of hexdigits; add examples (tests)

--- lib/Sah/Schema/hexbuf.pm
+++ lib/Sah/Schema/hexbuf.pm
@@ -1,11 +1,19 @@
 package Sah::Schema::hexbuf;
 
+# AUTHORITY
 # DATE
+# DIST
 # VERSION
 
 our $schema = [str => {
     summary => 'Binary data encoded in hexdigits',
-    match => qr/\A[0-9A-fa-f]+\z/,
+    match => qr/\A([0-9A-fa-f][0-9A-fa-f])*\z/,
+    examples => [
+        {data=>'', valid=>1},
+        {data=>'f', valid=>0, summary=>'Odd number of digits'},
+        {data=>'fafafa', valid=>1},
+        {data=>'fafafg', valid=>0},
+    ],
 }, {}];
 
 1;
