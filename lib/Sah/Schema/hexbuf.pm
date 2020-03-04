a2ea289fc74a4bb39343285a7ee81ebf92c9c8ca
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-03-04 11:49:50 +0700

    Update examples, use 'value' instead of 'data'

--- lib/Sah/Schema/hexbuf.pm
+++ lib/Sah/Schema/hexbuf.pm
@@ -9,10 +9,10 @@ our $schema = [str => {
     summary => 'Binary data encoded in hexdigits',
     match => qr/\A([0-9A-fa-f][0-9A-fa-f])*\z/,
     examples => [
-        {data=>'', valid=>1},
-        {data=>'f', valid=>0, summary=>'Odd number of digits'},
-        {data=>'fafafa', valid=>1},
-        {data=>'fafafg', valid=>0},
+        {value=>'', valid=>1},
+        {value=>'f', valid=>0, summary=>'Odd number of digits'},
+        {value=>'fafafa', valid=>1},
+        {value=>'fafafg', valid=>0},
     ],
 }, {}];
 
